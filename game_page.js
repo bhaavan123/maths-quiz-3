1fb4b90cd7e83c8dbb29711ce71344e516520d6d
bhaavan123 <81519012+bhaavan123@users.noreply.github.com>
2021-08-31 19:46:33 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,28 @@
+player1_name=localStorage.setItem("player1_name");
+player2_name=localStorage.setItem("player2_name");
+
+player1_score=0;
+player2_score=0;
+
+document.getElementById("player1_name").innerHTML = player1_name+ ": ";
+document.getElementById("player2_name").innerHTML = player2_name+ ": ";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("player_questions").innerHTML ="Question turn - "+player1_name;
+document.getElementById("player_answer").innerHTML ="answer turn - "+player1_name;
+
+function send() {
+    number1=document.getElementById("number_1").value;
+    number2=document.getElementById("number_2").value;
+    actual_answer=parseInt(number_1)*parseInt(number_2);
+    question_word="<h4 id='word_display'>"+number1+ "x"+number2+"</h4>";
+    input_box="<br>Answer : <input type='text' id='input_checkbox'>";
+    check_button="<button onclick='check()' class='btn btn-info'>check</button>";
+    row=question_word+input_box+check_button;
+    document.getElementById("output").innerHTML=row;
+    document.getElementById("number1").value= "";
+    document.getElementById("number2").value= "";
+}
+
